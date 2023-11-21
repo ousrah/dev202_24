4b3171d095b5ae0e7dd8d45058fecd0f487c5966
oussama rahmouni <ousrah@hotmail.com>
2023-11-21 15:17:12 +0100

    serie curseurs

--- 1 - bases de données/13 - les curseurs/cours.sql
+++ 1 - bases de données/13 - les curseurs/cours.sql
@@ -73,9 +73,46 @@ call insert_pilotes();
 select * from vol;
 
 
-
+delete from vol;
 #Exercice 2
 #videz la table vol
 #on souhaite insérer  pour chaque pilote des vols de sa ville de résidence a 
 #casablanca sur chaqune des avions dans la date en cours
 #utiliser les curseurs imbriqués
+
+drop procedure if exists insert_Vols_Res;
+delimiter $$
+create procedure insert_Vols_Res()
+begin 
+	declare np int;
+    declare villeP varchar(50);
+    declare flag1 boolean default false;
+    declare c1 cursor for select numpilote,villepilote from pilote;
+    declare continue handler for not found set flag1 = true;
+    open c1;
+		b1: loop
+			fetch c1 into np,villeP;
+			if flag1 then
+				leave b1;
+			end if;
+			begin
+                declare nv int;
+				declare flag2 boolean default false;
+				declare c2 cursor for select numav from avion;
+				declare continue handler for not found set flag2 = true;
+                open c2;
+					b2: loop
+						fetch c2 into nv;
+						if flag2 then
+							leave b2;
+						end if;
+                        insert into vol values (null,villeP,'Casablanca',current_date(),current_date(),np,nv);
+					end loop b2;
+                close c2;
+            end;
+        end loop b1;
+    close c1;
+end $$
+delimiter ;
+call insert_Vols_Res();
+select * from vol;
