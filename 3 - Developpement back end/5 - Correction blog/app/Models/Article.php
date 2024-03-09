1ad69ce5d397f1940498716c202270ed49c0d674
rahmouni oussama <ousrah@hotmail.com>
2024-03-09 08:32:33 +0100

    exercice blog 2

---
+++ 3 - Developpement back end/5 - Correction blog/app/Models/Article.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+    ];
+    
+    public function comments(){
+        return $this->morphMany(Comment::class, 'commenttable');
+    }
+}
